9469fc1c3786d5c670540cb5e1dec8e87d2e941b
vivek <viveksr012@gmail.com>
2025-04-21 11:11:45 +0530

    rk 2nd

--- differential_eqn.py
+++ differential_eqn.py
@@ -51,3 +51,35 @@ x,y = runge_kutta_method(equation,x0,y0,h,n)
 print(f"y(0.1) = {y[1]}")
 print(f"y(0.2) = {y[2]}")
 
+
+
+#rk method 
+def runge_kutta_2nd_order(f, x0, y0, h, n):
+    x = [x0]
+    y = [y0]
+    
+    for i in range(n):
+        k1 = h * f(x[-1], y[-1])
+        k2 = h * f(x[-1] + h, y[-1] + k1)
+        
+        y_next = y[-1] + (k1 + k2) / 2
+        x_next = x[-1] + h
+        
+        x.append(x_next)
+        y.append(y_next)
+    
+    return x, y
+
+def equation(x, y):
+    return y - x
+
+
+x0 = 0
+y0 = 2
+h = 0.1
+n = 2
+
+x, y = runge_kutta_2nd_order(equation, x0, y0, h, n)
+
+print(f"y(0.1) = {y[1]}")
+print(f"y(0.2) = {y[2]}")
