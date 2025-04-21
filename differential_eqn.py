cb0ecab5a9bdbf3f029ef541cf1e7f133a8fe2db
vivek <viveksr012@gmail.com>
2025-04-21 11:45:27 +0530

    assa

--- differential_eqn.py
+++ differential_eqn.py
@@ -1,61 +1,61 @@
 import matplotlib.pyplot as plt 
 import math as M
-# def euler_method(f, x0, y0, h, n):
-#     x = [x0]
-#     y = [y0]
+def euler_method(f, x0, y0, h, n):
+    x = [x0]
+    y = [y0]
     
-#     for i in range(n):
-#         y_next = y[-1] + h * f(x[-1], y[-1])
-#         x_next = x[-1] + h
-#         x.append(x_next)
-#         y.append(y_next)
+    for i in range(n):
+        y_next = y[-1] + h * f(x[-1], y[-1])
+        x_next = x[-1] + h
+        x.append(x_next)
+        y.append(y_next)
     
-#     return x, y
+    return x, y
 
-# def equation(x, y):
-#     return y - x
+def equation(x, y):
+    return y - x
 
-# x0 = 0
-# y0 = 2
-# h = 0.1
-# n = 2  
+x0 = 0
+y0 = 2
+h = 0.1
+n = 2  
 
-# x, y = euler_method(equation, x0, y0, h, n)
+x, y = euler_method(equation, x0, y0, h, n)
 
-# print(f"y(0.1) = {y[1]}")
-# print(f"y(0.2) = {y[2]}")
+print(f"y(0.1) = {y[1]}")
+print(f"y(0.2) = {y[2]}")
 
 # rk - 4 
 # qus 2
-# def runge_kutta_method(f, x0, y0, h, n):
-#     x = [x0]
-#     y = [y0]
+def runge_kutta_method(f, x0, y0, h, n):
+    x = [x0]
+    y = [y0]
     
-#     for i in range(n):
-#         k1 = h * f(x[-1], y[-1])
-#         k2 = h * f(x[-1] + h / 2, y[-1] + k1 / 2)
-#         k3 = h * f(x[-1] + h / 2, y[-1] + k2 / 2)
-#         k4 = h * f(x[-1] + h, y[-1] + k3)
+    for i in range(n):
+        k1 = h * f(x[-1], y[-1])
+        k2 = h * f(x[-1] + h / 2, y[-1] + k1 / 2)
+        k3 = h * f(x[-1] + h / 2, y[-1] + k2 / 2)
+        k4 = h * f(x[-1] + h, y[-1] + k3)
         
-#         y_next = y[-1] + (k1 + 2 * k2 + 2 * k3 + k4) / 6
-#         x_next = x[-1] + h
+        y_next = y[-1] + (k1 + 2 * k2 + 2 * k3 + k4) / 6
+        x_next = x[-1] + h
         
-#         x.append(x_next)
-#         y.append(y_next)
+        x.append(x_next)
+        y.append(y_next)
     
-#     return x, y
-# def equation(x, y):
-#     return y + x*M.exp(x)
-# x0 = 0
-# y0 = 1
-# h = 0.1
-# n = 20
-# x,y = runge_kutta_method(equation,x0,y0,h,n)
-# print(f"y(2) ={y[n]}")
+    return x, y
+def equation(x, y):
+    return y + x*M.exp(x)
+x0 = 0
+y0 = 1
+h = 0.1
+n = 20
+x,y = runge_kutta_method(equation,x0,y0,h,n)
+print(f"y(2) ={y[n]}")
 
-# plt.plot(x,y)
-# plt.scatter(x,y)
-# plt.show()
+plt.plot(x,y)
+plt.scatter(x,y)
+plt.show()
 
 #qus 3
 def runge_kutta_method(f, x0, y0, h, n):
@@ -89,32 +89,32 @@ plt.scatter(x,y)
 plt.show()
 
 #rk method 
-# def runge_kutta_2nd_order(f, x0, y0, h, n):
-#     x = [x0]
-#     y = [y0]
+def runge_kutta_2nd_order(f, x0, y0, h, n):
+    x = [x0]
+    y = [y0]
     
-#     for i in range(n):
-#         k1 = h * f(x[-1], y[-1])
-#         k2 = h * f(x[-1] + h, y[-1] + k1)
+    for i in range(n):
+        k1 = h * f(x[-1], y[-1])
+        k2 = h * f(x[-1] + h, y[-1] + k1)
         
-#         y_next = y[-1] + (k1 + k2) / 2
-#         x_next = x[-1] + h
+        y_next = y[-1] + (k1 + k2) / 2
+        x_next = x[-1] + h
         
-#         x.append(x_next)
-#         y.append(y_next)
+        x.append(x_next)
+        y.append(y_next)
     
-#     return x, y
+    return x, y
 
-# def equation(x, y):
-#     return y - x
+def equation(x, y):
+    return y - x
 
 
-# x0 = 0
-# y0 = 2
-# h = 0.1
-# n = 2
+x0 = 0
+y0 = 2
+h = 0.1
+n = 2
 
-# x, y = runge_kutta_2nd_order(equation, x0, y0, h, n)
+x, y = runge_kutta_2nd_order(equation, x0, y0, h, n)
 
-# print(f"y(0.1) = {y[1]}")
-# print(f"y(0.2) = {y[2]}")
+print(f"y(0.1) = {y[1]}")
+print(f"y(0.2) = {y[2]}")
