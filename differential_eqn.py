f27a76eb7ea2b7182c721df034c7c94b05b57839
vivek <viveksr012@gmail.com>
2025-04-21 11:41:49 +0530

    add

--- differential_eqn.py
+++ differential_eqn.py
@@ -26,7 +26,38 @@ import math as M
 # print(f"y(0.2) = {y[2]}")
 
 # rk - 4 
-#qus 2
+# qus 2
+# def runge_kutta_method(f, x0, y0, h, n):
+#     x = [x0]
+#     y = [y0]
+    
+#     for i in range(n):
+#         k1 = h * f(x[-1], y[-1])
+#         k2 = h * f(x[-1] + h / 2, y[-1] + k1 / 2)
+#         k3 = h * f(x[-1] + h / 2, y[-1] + k2 / 2)
+#         k4 = h * f(x[-1] + h, y[-1] + k3)
+        
+#         y_next = y[-1] + (k1 + 2 * k2 + 2 * k3 + k4) / 6
+#         x_next = x[-1] + h
+        
+#         x.append(x_next)
+#         y.append(y_next)
+    
+#     return x, y
+# def equation(x, y):
+#     return y + x*M.exp(x)
+# x0 = 0
+# y0 = 1
+# h = 0.1
+# n = 20
+# x,y = runge_kutta_method(equation,x0,y0,h,n)
+# print(f"y(2) ={y[n]}")
+
+# plt.plot(x,y)
+# plt.scatter(x,y)
+# plt.show()
+
+#qus 3
 def runge_kutta_method(f, x0, y0, h, n):
     x = [x0]
     y = [y0]
@@ -45,18 +76,17 @@ def runge_kutta_method(f, x0, y0, h, n):
     
     return x, y
 def equation(x, y):
-    return y + x*M.exp(x)
+    return 2*(25-y)
 x0 = 0
-y0 = 1
+y0 = 40
 h = 0.1
-n = 20
+n = 100
 x,y = runge_kutta_method(equation,x0,y0,h,n)
-print(f"y(2) ={y[20]}")
+print(f"y(10) ={y[-1]}")
 
 plt.plot(x,y)
 plt.scatter(x,y)
 plt.show()
-
 
 #rk method 
 # def runge_kutta_2nd_order(f, x0, y0, h, n):
